8580f054276a58f4529dea292a65c54e00f7474a
Kiselev Andrei <annikkis@yandex.ru>
2025-05-30 01:09:19 +0200

    feat: add header
    add layout component "Header"

---
+++ src/components/layouts/header/index.tsx
@@ -0,0 +1,8 @@
+import { HTMLAttributes } from 'react'
+import { clsx } from 'clsx/lite'
+
+export interface HeaderProps extends HTMLAttributes<HTMLDivElement> {}
+
+export const Header = ({className, children}: HeaderProps) => (
+  <header className={clsx('flex bg-header-background', className)}>{children}</header>
+)
