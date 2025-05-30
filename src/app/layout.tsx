b1c1cb4f5e49eeb64a790a86a80b5f584d50bdce
Kiselev Andrei <annikkis@yandex.ru>
2025-05-30 12:07:03 +0200

    refactor: edit layouts and format HomePage
    remove unused components
    format HomePage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,11 +4,6 @@ import { Header } from '@/components/layouts/header'
 import { Navbar } from '@/components/layouts/navbar'
 import { Footer } from '@/components/layouts/footer'
 import './globals.css'
-<<<<<<< HEAD
-=======
-import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
->>>>>>> dev
 
 const fontNunitoSans = Nunito_Sans({
   variable: '--font-nunito-sans',
@@ -30,7 +25,6 @@ export default function RootLayout({ children }: LayoutProps) {
         <main className="flex grow">{children}</main>
         <Footer />
       </body>
-      <Footer />
     </html>
   )
 }
