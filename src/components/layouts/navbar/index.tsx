55b7e66815beffd7e8316be7235c74fcc86e4080
zhivkoGrchev <z.grcev@gmail.com>
2025-06-02 21:28:50 +0200

    Logo is added to the NavBarand bg-image is moved to public dir

--- src/components/layouts/navbar/index.tsx
+++ src/components/layouts/navbar/index.tsx
@@ -1,10 +1,18 @@
 import { APP_NAME } from '@/constants'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export const Navbar = () => (
   <nav className="container mx-auto flex justify-between items-center p-4">
-    <Link href="/" className="text-white text-2xl font-bold">
-      {APP_NAME}
+    <Link href="/" className="flex items-center gap-2">
+      <Image
+        src="/logo.png" // Path to your logo in the public folder
+        alt={APP_NAME}
+        width={77} // Desired width
+        height={50} // Desired height
+        className="object-contain" // Keeps aspect ratio
+      />
+      <span className="text-white text-2xl font-bold">{APP_NAME}</span>
     </Link>
     <div className="flex space-x-4">
       <Link
