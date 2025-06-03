4b5a3151cfd74f797a604727c36ab7131c15a89f
zhivkoGrchev <z.grcev@gmail.com>
2025-06-03 11:26:22 +0200

    images folder created

--- src/components/layouts/header/index.tsx
+++ src/components/layouts/header/index.tsx
@@ -1,8 +1,6 @@
-import { HTMLAttributes } from 'react'
+import type { HTMLAttributes } from 'react'
 import { clsx } from 'clsx/lite'
 
 export interface HeaderProps extends HTMLAttributes<HTMLDivElement> {}
 
-export const Header = ({className, children}: HeaderProps) => (
-  <header className={clsx('flex bg-header-background', className)}>{children}</header>
-)
+export const Header = ({ className, children }: HeaderProps) => <header className={clsx('flex bg-header-background', className)}>{children}</header>
--- src/components/layouts/navbar/index.tsx
+++ src/components/layouts/navbar/index.tsx
@@ -11,10 +11,10 @@ export const Navbar = () => {
     <nav className="container mx-auto flex justify-between items-center p-4">
       <Link href="/" className="flex items-center gap-2">
         <Image
-          src="/logo.png"  // Path from public folder
-          alt={APP_NAME}          // Accessibility
-          width={77}              // Desired display width
-          height={50}             // Desired display height
+          src="/images/logo.png" // Path from public folder
+          alt={APP_NAME} // Accessibility
+          width={77} // Desired display width
+          height={50} // Desired display height
           className="object-contain" // Keep aspect ratio
         />
         <span className=" text-cyan-50 text-2xl font-bold">{APP_NAME}</span>
