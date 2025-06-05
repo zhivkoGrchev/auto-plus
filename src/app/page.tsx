7daf070d67f70a7bfd6169963b93f81a1d0c0dd6
Georgi Georgiev <georgigeorgiev@iMac-von-Georgi.fritz.box>
2025-06-05 19:25:54 +0200

    refactored

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
-import { useTranslations } from 'next-intl'
 import Hero from '@/components/hero'
-import heroImg from '/public/images/bg-image.jpg'
 
 export default function HomePage() {
-  const t = useTranslations('HomePage')
-  return <Hero imgData={heroImg} imgAlt="car background" title={t('title')} description={t('description')} startDemo={t('startDemo')} />
+  return <Hero />
 }
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,27 +1,22 @@
-import type { StaticImageData } from 'next/image'
 import Link from 'next/link'
 import Image from 'next/image'
+import { useTranslations } from 'next-intl'
+import heroImg from '/public/images/bg-image.jpg'
 
-interface HeroProps {
-  imgData: StaticImageData
-  imgAlt: string
-  title: string
-  description: string
-  startDemo: string
-}
+export default function Hero() {
+  const t = useTranslations('Hero')
 
-export default function Hero(props: HeroProps) {
   return (
     <div className="relative text-cyan-50 flex flex-col items-center justify-center h-screen w-full">
       <div className="absolute -z-10 inset-0">
-        <Image src={props.imgData} alt={props.imgAlt} fill style={{ objectFit: 'cover' }} />
+        <Image src={heroImg} alt={t('imgAlt')} fill style={{ objectFit: 'cover' }} />
       </div>
-      <div className="absolute inset-0 bg-gradient-to-r from-slate-900" />
+      <div className="absolute inset-0" />
       <div className="text-center px-4 z-10 mb-auto pt-30">
-        <h1 className="text-cyan-50 text-4xl">{props.title}</h1>
-        <h2 className="mb-10 text-cyan-50 text-2xl">{props.description}</h2>
+        <h1 className="text-cyan-50 text-4xl">{t('title')}</h1>
+        <h2 className="mb-10 text-cyan-50 text-2xl">{t('description')}</h2>
         <Link href="/demo" className="ml-12 mt-5 bg-cyan-600 text-cyan-50 px-4 py-1 rounded-md font-medium hover:bg-cyan-400 transition-colors">
-          {props.startDemo}
+          {t('startDemo')}
         </Link>
       </div>
     </div>
