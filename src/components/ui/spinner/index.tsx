725e965cea9081288db45aac97efbc78a1f9b934
Kiselev Andrei <annikkis@yandex.ru>
2025-06-07 00:47:27 +0200

    refactor: delete all self-written components
    Delete: Slot, Button, Overlay, Spinner
    Delete types for Button

--- src/components/ui/spinner/index.tsx
+++
@@ -1,8 +0,0 @@
-import { clsx } from 'clsx/lite'
-import { LuLoaderCircle } from 'react-icons/lu'
-
-type SpinnerProps = React.SVGAttributes<SVGElement> & {
-  className?: string
-}
-
-export const Spinner = ({ className, ...props }: SpinnerProps) => <LuLoaderCircle className={clsx('animate-spin', className)} {...props} />
