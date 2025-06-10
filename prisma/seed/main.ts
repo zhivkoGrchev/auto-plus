c1fb9516eceb2ae74969c65ed435a545f37fda40
Georgi Georgiev <georgigeorgiev@iMac-von-Georgi.fritz.box>
2025-06-10 18:39:18 +0200

    removed unnecessary comments

--- prisma/seed/main.ts
+++ prisma/seed/main.ts
@@ -4,20 +4,16 @@ import data from './data'
 async function main() {
   const prisma = new PrismaClient()
   try {
-    // Remove existing data to avoid duplicates
     await prisma.carModel.deleteMany({})
     await prisma.carMake.deleteMany({})
 
-    // Seed car makes and models
     for (const model of data.carModels) {
-      // Create car make
       const carMake = await prisma.carMake.create({
         data: {
           name: model.make.name,
         },
       })
 
-      // Create the car model with the connected make
       await prisma.carModel.create({
         data: {
           name: model.name,
