8b25bb45cbfad125e4cb2fb4c946608a0d0ef09e
gjorgiev <georgi.coding@proton.me>
2025-08-03 21:16:07 +0200

    fixed translations in server action validation

--- src/lib/validators.ts
+++ src/lib/validators.ts
@@ -1,25 +1,31 @@
 import { z } from 'zod'
 import { Transmission } from './generated/prisma'
 import { FuelType } from './generated/prisma'
+import { getTranslations } from 'next-intl/server'
 
-export const insertCarSchema = z.object({
-  brandId: z.string().min(1, 'Brand is required'),
-  modelId: z.string().min(1, 'Model is required'),
-  year: z.number().int().min(1886, 'Year must be 1886 or later').max(new Date().getFullYear(), 'Year cannot be in the future'),
-  color: z.string().min(1, 'Color is required'),
-  transmission: z.nativeEnum(Transmission, {
-    errorMap: () => {
-      return { message: 'Please select transmission type' }
-    },
-  }),
-  fuelType: z.nativeEnum(FuelType, {
-    errorMap: () => {
-      return { message: 'Please select fuel type' }
-    },
-  }),
-  mileage: z.number().int().min(1, 'Mileage is required'),
-  vin: z.string().optional(),
-  price: z.number().int().min(1, 'Price is required'),
-  description: z.string().optional(),
-})
-export type InsertCarSchema = z.infer<typeof insertCarSchema>
+export const createInsertCarSchema = async () => {
+  const t = await getTranslations('Validations')
+  
+  return z.object({
+    brandId: z.string().min(1, t('brand')),
+    modelId: z.string().min(1, t('model')),
+    year: z.number().int().min(1886, t('yearMin')).max(new Date().getFullYear(), t('yearMax')),
+    color: z.string().min(1, t('color')),
+    transmission: z.nativeEnum(Transmission, {
+      errorMap: () => {
+        return { message: t('transmission') }
+      },
+    }),
+    fuelType: z.nativeEnum(FuelType, {
+      errorMap: () => {
+        return { message: t('fuelType') }
+      },
+    }),
+    mileage: z.number().int().min(1, t('mileage')),
+    vin: z.string().optional(),
+    price: z.number().int().min(1, t('price')),
+    description: z.string().optional(),
+  })
+}
+
+export type InsertCarSchema = z.infer<Awaited<ReturnType<typeof createInsertCarSchema>>>
