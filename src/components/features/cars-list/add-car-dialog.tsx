3e392f2c3979a684aa09626b4bed04cc51cb2250
Kiselev Andrei <annikkis@yandex.ru>
2025-08-11 00:46:08 +0200

    remove unused imports

--- src/components/features/cars-list/add-car-dialog.tsx
+++ src/components/features/cars-list/add-car-dialog.tsx
@@ -3,20 +3,8 @@
 import { type ChangeEvent, type MouseEvent, useState, useTransition } from 'react'
 import { useTranslations } from 'next-intl'
 import { FaCheck, FaImage, FaPlus, FaSpinner } from 'react-icons/fa'
-import { MdClose } from 'react-icons/md'
 import { FuelType, Transmission } from '@prisma/client'
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogOverlay,
-  DialogPortal,
-  DialogTitle,
-  DialogTrigger,
-} from '@/components/ui/dialog'
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
 import { Button } from '@/components/ui/button'
 import { Label } from '@/components/ui/label'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
