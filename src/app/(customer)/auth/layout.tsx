ccb2128389dd4ae8aa58a2000744ed6c90feebee
Andrei Kiselev <dev.akprog@gmail.com>
2025-11-02 22:57:16 +0100

    Rename CustomerAdminLayout and CustomerAuthLayout

--- src/app/(customer)/auth/layout.tsx
+++ src/app/(customer)/auth/layout.tsx
@@ -3,7 +3,7 @@ import { Header } from '@/components/layouts/header'
 import { Navbar } from '@/components/layouts/navbar'
 import { ToolBar } from '@/components/widgets/toolbar'
 
-export default function CustomerLayout({ children }: LayoutProps) {
+export default function CustomerAuthLayout({ children }: LayoutProps) {
   return (
     <>
       <Header>
