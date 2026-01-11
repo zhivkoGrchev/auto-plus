bb39d6566e8f408d90bbe595ed52badf1b6cf716
Kiselev Andrei <annikkis@yandex.ru>
2026-01-12 00:22:33 +0100

    refactor(AdminPage): refactor admin page

--- src/app/(customer)/admin/website/page.tsx
+++
@@ -1,8 +0,0 @@
-import { getProfile } from '@/lib/actions/profile.actions'
-import { WebsiteAdmin } from '@/components/features/website-admin'
-
-export default async function WebsitePage() {
-  const { data: profile, error } = await getProfile()
-  const slug = !error ? profile.slug : null
-  return <WebsiteAdmin profileSlug={slug} />
-}
