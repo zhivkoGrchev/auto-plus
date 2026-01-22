a2a5ab740a92b2a8b4fff18ac11a216bdd638ed9
Kiselev Andrei <annikkis@yandex.ru>
2026-01-23 00:21:08 +0100

    feat: add resize image

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,4 @@
-import { clsx, type ClassValue } from 'clsx'
+import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
@@ -14,6 +14,59 @@ export function getChangedFields<T extends Record<string, any>>(original: T, upd
 
 export function toJson<T>(data: T): T {
   return JSON.parse(JSON.stringify(data))
+}
+
+export function resizeImage(file: File, maxWidth = 800, maxHeight = 600, quality = 0.8): Promise<File> {
+  return new Promise((resolve, reject) => {
+    const canvas = document.createElement('canvas')
+    const ctx = canvas.getContext('2d')
+    const img = new Image()
+
+    if (!ctx) {
+      reject(new Error('Could not get canvas context'))
+      return
+    }
+
+    img.onload = () => {
+      let { width, height } = img
+      if (width > height) {
+        if (width > maxWidth) {
+          height = (height * maxWidth) / width
+          width = maxWidth
+        }
+      } else {
+        if (height > maxHeight) {
+          width = (width * maxHeight) / height
+          height = maxHeight
+        }
+      }
+
+      canvas.width = width
+      canvas.height = height
+      ctx.drawImage(img, 0, 0, width, height)
+      canvas.toBlob(
+        (blob) => {
+          if (blob) {
+            const resizedFile = new File([blob], file.name, {
+              type: file.type,
+              lastModified: Date.now(),
+            })
+            resolve(resizedFile)
+          } else {
+            reject(new Error('Failed to create resized image blob'))
+          }
+        },
+        file.type,
+        quality
+      )
+    }
+
+    img.onerror = () => {
+      reject(new Error('Failed to load image'))
+    }
+
+    img.src = URL.createObjectURL(file)
+  })
 }
 
 export const normalize = (s: unknown) =>
