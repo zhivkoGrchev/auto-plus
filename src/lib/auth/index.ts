52f07ea7716f23acb4158135210bdfd25a3748ab
zhivkoGrchev <149097390+zhivkoGrchev@users.noreply.github.com>
2026-02-16 20:46:25 +0100

    Update index.ts

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -5,6 +5,7 @@ import { prisma } from '@/prisma'
 
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_BASE_URL,
+  secret: process.env.BETTER_AUTH_SECRET,
   database: prismaAdapter(prisma, {
     provider: 'postgresql',
   }),
