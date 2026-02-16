6947afc0ff09aba8c7f561242cf927d7f22f94ab
zhivkoGrchev <149097390+zhivkoGrchev@users.noreply.github.com>
2026-02-16 20:38:15 +0100

    Update client.ts

--- src/lib/auth/client.ts
+++ src/lib/auth/client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from 'better-auth/react'
 
 export const authClient = createAuthClient({
-  baseURL: process.env.BETTER_AUTH_URL,
+  baseURL: process.env.BETTER_AUTH_BASE_URL,
 })
 
 export const { signUp, signIn, signOut, getSession, useSession } = authClient
